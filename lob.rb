6b7fcf08a72a4b8e94af807a19a6f61578e42e86
CrowdHailer <peterhsaxton@gmail.com>
2015-11-09 16:41:23 +0000

    add form for channel selection

--- lob.rb
+++ lob.rb
@@ -4,6 +4,18 @@ get '/' do
   erb :index
 end
 
+post '/join' do
+  channel = request.POST["channel"]
+  if channel == ""
+    channel = "default"
+  end
+  if request.POST["flyer"]
+    redirect "/#{channel}/flyer"
+  else
+    redirect "/#{channel}/tracker"
+  end
+end
+
 get '/:channel_name/flyer' do
   erb :flyer, locals: {channel_name: params["channel_name"]}
 end
