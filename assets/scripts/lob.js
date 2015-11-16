db109835cabdfa97b8f8dacc622ef1d406b3dc05
CrowdHailer <peterhsaxton@gmail.com>
2015-11-16 11:11:49 +0000

    separate acceleration service

--- assets/scripts/lob.js
+++ assets/scripts/lob.js
@@ -1,32 +1,6 @@
 /*jshint esnext: true */
 
-function deviceUsesInvertedAcceleration(userAgent) {
-  if (userAgent.match(/Windows/i)) {
-    return true;
-  } else if (userAgent.match(/Android/i)) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function lookupAccelerationVectorRectifyForDevice(userAgent, console) {
-  var invert = deviceUsesInvertedAcceleration(userAgent);
-
-  if (invert) {
-    console.log("Device uses inverted acceleration. UserAgent: \"" + userAgent + "\"");
-    return function invertVector(vector) {
-      return {
-        x: - 1 * vector.x,
-        y: - 1 * vector.y,
-        z: - 1 * vector.z,
-      };
-    };
-  } else {
-    console.log("Device uses standard acceleration. UserAgent: \"" + userAgent + "\"");
-    return function identity(vector) { return vector; };
-  }
-}
+import { lookupAccelerationVectorRectifyForDevice } from "./lob/acceleration";
 
 console.log("Starting Lob script");
 var rectifyAcceleration = lookupAccelerationVectorRectifyForDevice(navigator.userAgent, window.console);
@@ -47,9 +21,9 @@ ready(function () {
 
   var $flyer = querySelector("#orientation-generator", document);
   console.log($flyer);
-  
+
   if (window.DeviceMotionEvent) {
-    $flyer.addEventListener("devicemotion", deviceMotionHandler);
+    window.addEventListener("devicemotion", deviceMotionHandler);
   }
 });
 
--- public/lob.js
+++ public/lob.js
@@ -2,6 +2,36 @@
 
   /*jshint esnext: true */
 
+  function deviceUsesInvertedAcceleration(userAgent) {
+    if (userAgent.match(/Windows/i)) {
+      return true;
+    } else if (userAgent.match(/Android/i)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function lookupAccelerationVectorRectifyForDevice(userAgent, console) {
+    var invert = deviceUsesInvertedAcceleration(userAgent);
+
+    if (invert) {
+      console.log("Device uses inverted acceleration. UserAgent: \"" + userAgent + "\"");
+      return function invertVector(vector) {
+        return {
+          x: - 1 * vector.x,
+          y: - 1 * vector.y,
+          z: - 1 * vector.z,
+        };
+      };
+    } else {
+      console.log("Device uses standard acceleration. UserAgent: \"" + userAgent + "\"");
+      return function identity(vector) { return vector; };
+    }
+  }
+
+  /*jshint esnext: true */
+
   function throttle(fn, threshhold, scope) {
       threshhold = threshhold || 250;
       var last,
@@ -46,36 +76,6 @@
     publish: publish
   };
 
-  /*jshint esnext: true */
-
-  function deviceUsesInvertedAcceleration(userAgent) {
-    if (userAgent.match(/Windows/i)) {
-      return true;
-    } else if (userAgent.match(/Android/i)) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  function lookupAccelerationVectorRectifyForDevice(userAgent, console) {
-    var invert = deviceUsesInvertedAcceleration(userAgent);
-
-    if (invert) {
-      console.log("Device uses inverted acceleration. UserAgent: \"" + userAgent + "\"");
-      return function invertVector(vector) {
-        return {
-          x: - 1 * vector.x,
-          y: - 1 * vector.y,
-          z: - 1 * vector.z,
-        };
-      };
-    } else {
-      console.log("Device uses standard acceleration. UserAgent: \"" + userAgent + "\"");
-      return function identity(vector) { return vector; };
-    }
-  }
-
   console.log("Starting Lob script");
   var rectifyAcceleration = lookupAccelerationVectorRectifyForDevice(navigator.userAgent, window.console);
 
@@ -92,9 +92,9 @@
 
     var $flyer = querySelector("#orientation-generator", document);
     console.log($flyer);
-    
+
     if (window.DeviceMotionEvent) {
-      $flyer.addEventListener("devicemotion", deviceMotionHandler);
+      window.addEventListener("devicemotion", deviceMotionHandler);
     }
   });
 
