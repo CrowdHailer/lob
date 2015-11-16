44075f11e6e9b7e4f31b583adf9364bb9ff60f2d
CrowdHailer <peterhsaxton@gmail.com>
2015-11-16 17:00:16 +0000

    streak tested

--- assets/scripts/lob/util.js
+++ assets/scripts/lob/util.js
@@ -23,5 +23,20 @@ export function throttle(fn, threshhold, scope) {
 }
 
 export function streak(predicate, collection) {
-  return [];
+  var current_streak = [];
+  var output = [];
+  collection.forEach(function (item) {
+    if (predicate(item)) {
+      current_streak.push(item);
+    } else {
+      if (current_streak.length !== 0) {
+        output.push(current_streak);
+      }
+      current_streak = [];
+    }
+  });
+  if (current_streak.length !== 0) {
+    output.push(current_streak);
+  }
+  return output;
 }
--- test/streak_test.js
+++ test/streak_test.js
@@ -2,12 +2,31 @@
 
 import { streak } from "../assets/scripts/lob/util.js";
 
-
 describe("Collection streak function", function() {
 
-  it("It should return an empty list from an empty source", function() {
-    var output = streak([], function(){ return true; });
+  it("it should return an empty list from an empty source", function() {
+    var output = streak(function(){ return true; }, []);
     expect(output).toEqual([]);
+  });
+
+  it("it should return an empty list for collection with none passing predicate", function() {
+    var output = streak(function(){ return false; }, [2, 3]);
+    expect(output).toEqual([]);
+  });
+
+  it("it should return single streak for passing collection", function () {
+    var output = streak(function(){ return true; }, [1, 2, 3]);
+    expect(output).toEqual([[1, 2, 3]]);
+  });
+
+  it("it should stop when predicate no longer true", function () {
+    var output = streak(function(i){ return i < 3; }, [1, 2, 3]);
+    expect(output).toEqual([[1, 2]]);
+  });
+
+  it("it should pass multiple streaks", function () {
+    var output = streak(function(i){ return i < 3; }, [1, 2, 3, 1, 2, 3]);
+    expect(output).toEqual([[1, 2], [1, 2]]);
   });
 
 });
