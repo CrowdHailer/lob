0d084c02a1b6fcf86b18d5921e7566b6b0754628
CrowdHailer <peterhsaxton@gmail.com>
2015-11-19 15:47:45 +0000

    jasmine type declaration

--- test/lob_test.ts
+++ test/lob_test.ts
@@ -1,7 +1,15 @@
+/// <reference path="./jasmine.d.ts" />
 /*jshint esnext: true */
 
 describe("Lob", function() {
 
+  describe("Avionics", function () {
+
+    it("should start pending", function () {
+      // var avionics = Avionics.new();
+      // expect(avionics.state).toBe(Avionics.PENDING);
+    });
+  });
   it("should pass a dummy test", function() {
     expect(true).toEqual(true);
   });
