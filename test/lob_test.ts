11b587fd9fc1e8a02e74c783fe68dcb80d8e7915
CrowdHailer <peterhsaxton@gmail.com>
2015-11-19 16:07:48 +0000

    start avionics

--- test/lob_test.ts
+++ test/lob_test.ts
@@ -1,15 +1,39 @@
 /// <reference path="./jasmine.d.ts" />
 /*jshint esnext: true */
 
+class Avionics{
+  status: String
+
+  start(){
+    this.status = Avionics.READING
+    return this;
+  }
+
+  static new(){
+    return new Avionics()
+  }
+  constructor(){
+    this.status = Avionics.READY
+  }
+  static READY = "READY"
+  static READING = "READING"
+}
+
 describe("Lob", function() {
 
   describe("Avionics", function () {
 
-    it("should start pending", function () {
-      // var avionics = Avionics.new();
-      // expect(avionics.state).toBe(Avionics.PENDING);
+    it("should be created ready", function () {
+      var avionics = Avionics.new();
+      expect(avionics.status).toBe(Avionics.READY);
+    });
+
+    it("should be reading after being started", function () {
+      var avionics = Avionics.new().start();
+      expect(avionics.status).toBe(Avionics.READING);
     });
   });
+
   it("should pass a dummy test", function() {
     expect(true).toEqual(true);
   });
