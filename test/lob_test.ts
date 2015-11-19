c6f49b6d2041a38f3b1a3296480f31b05d872a35
CrowdHailer <peterhsaxton@gmail.com>
2015-11-19 17:30:18 +0000

    start avionics state

--- test/lob_test.ts
+++ test/lob_test.ts
@@ -1,36 +1,65 @@
 /// <reference path="./jasmine.d.ts" />
 /*jshint esnext: true */
 
-class Avionics{
+interface AvionicsState{
   status: String
+  data: Array<any>
+  remainingTime: Number
+  // Time: Number
+}
 
-  start(){
-    this.status = Avionics.READING
-    return this;
+var AvionicsState = {
+  start: function(state: AvionicsState){ state.status = Avionics.READING; },
+  new: function (): AvionicsState {
+    return {status: Avionics.READY, data: [], remainingTime: Avionics.MAX_RECORDING_DURATION};
+  }
+}
+
+class Avionics{
+  state: AvionicsState
+
+  static start(avionics: Avionics){
+    AvionicsState.start(avionics.state)
+    return avionics;
   }
 
   static new(){
     return new Avionics()
   }
   constructor(){
-    this.status = Avionics.READY
+    this.state = AvionicsState.new()
   }
   static READY = "READY"
   static READING = "READING"
+  static MAX_RECORDING_DURATION = 20
 }
 
 describe("Lob", function() {
 
-  describe("Avionics", function () {
+  describe("AvionicsState", function () {
+    it("should start with no data", function () {
+      var state = AvionicsState.new();
+      expect(state.data.length).toBe(0);
+    });
 
-    it("should be created ready", function () {
-      var avionics = Avionics.new();
-      expect(avionics.status).toBe(Avionics.READY);
+    it("should all remaining flight time", function () {
+      var state = AvionicsState.new();
+      expect(state.remainingTime).toBe(Avionics.MAX_RECORDING_DURATION);
+    });
+  })
+  describe("Created Avionics", function () {
+    var avionics;
+    beforeEach(function(){
+      avionics = Avionics.new();
+    })
+
+    it("should be ready", function () {
+      expect(avionics.state.status).toBe(Avionics.READY);
     });
 
     it("should be reading after being started", function () {
-      var avionics = Avionics.new().start();
-      expect(avionics.status).toBe(Avionics.READING);
+      Avionics.start(avionics)
+      expect(avionics.state.status).toBe(Avionics.READING);
     });
   });
 
