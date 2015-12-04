09f59d914ef6e3b4e0ec042b70b2ea8dccff6067
CrowdHailer <peterhsaxton@gmail.com>
2015-12-04 12:39:31 +0000

    fix logger interface

--- assets/scripts/action.ts
+++ assets/scripts/action.ts
@@ -1,9 +1,13 @@
 import * as Dispatcher from "./dispatcher.ts";
 
+interface Logger {
+  info: (...items) => void;
+  error: (...items) => void;
+}
 var NullLogger = {info: function(...a){ null; }, error: function(...a){ null; }};
 
-export function create(filter: () => void, logger?): {(): void, register: (handler: ()=> void) => void}
-export function create<A, B>(filter: (a: A) => B, logger?): {(a: A): void, register: (handler: (m: B)=> void) => void}
+export function create(filter: () => void, logger?: Logger): {(): void, register: (handler: ()=> void) => void}
+export function create<A, B>(filter: (a: A) => B, logger?: Logger): {(a: A): void, register: (handler: (m: B)=> void) => void}
 export function create(filter, logger=NullLogger){
   var action: any;
   var dispatcher = Dispatcher.create();
