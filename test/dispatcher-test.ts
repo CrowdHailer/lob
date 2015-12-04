005e3de244b0d81f57927506ed5602b1627960f4
CrowdHailer <peterhsaxton@gmail.com>
2015-12-04 12:29:17 +0000

    setup action object

--- test/dispatcher-test.ts
+++ test/dispatcher-test.ts
@@ -10,6 +10,53 @@ function createTranscriptFunction(){
 };
 import * as Dispatcher from "../assets/scripts/dispatcher.ts";
 
+var identityFn = function(a) { return a; };
+var NullLogger = {info: function(...a){ null; }, error: function(...a){ null; }};
+
+function createAction(filter: () => void, logger?): {(): void, register: (handler: ()=> void) => void}
+function createAction<A, B>(filter: (a: A) => B, logger?): {(a: A): void, register: (handler: (m: B)=> void) => void}
+function createAction(filter, logger=NullLogger){
+  var action: any;
+  var dispatcher = Dispatcher.create();
+  action = function(minutiae){
+    try {
+      dispatcher.dispatch(filter(minutiae));
+    }
+    catch (e) {
+      logger.error(e);
+    }
+  };
+  action.register = function(handler){
+    dispatcher = dispatcher.register(handler);
+  };
+  return action;
+};
+
+describe("Action", function(){
+  it("should pass minutiae to dispatcher", function(){
+    var action = createAction(identityFn);
+    var handler = createTranscriptFunction();
+    action.register(handler);
+    action("some data");
+    expect(handler.transcript[0]).toEqual(["some data"]);
+  });
+
+  it("should pass details through filter", function(){
+    var action = createAction(function(_string: string){ return "some extended data"; });
+    var handler = createTranscriptFunction();
+    action.register(handler);
+    action("some data");
+    expect(handler.transcript[0]).toEqual(["some extended data"]);
+  });
+
+  it("should log error if filter raises exception", function(){
+    var logger = {info: createTranscriptFunction(), error: createTranscriptFunction()};
+    var action = createAction(function (){ throw new Error("bad filter"); }, logger);
+    action();
+    expect(logger.error.transcript[0]).toEqual([new Error("bad filter")]);
+  });
+});
+
 describe("Dispatcher", function(){
   it("should pass the minutiae (precise details) to a registered listener", function(){
     var handler = createTranscriptFunction();
