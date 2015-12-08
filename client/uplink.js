0f8fda7a9b7f8434a3fa1595c22bf1e0683174ab
CrowdHailer <peterhsaxton@gmail.com>
2015-12-08 16:45:31 +0000

    setup uplink details

--- client/uplink.js
+++ client/uplink.js
@@ -1,8 +1,33 @@
 /* jshint esnext: true */
 
+import { getQueryParameter } from "./utils/location";
+
 export default function(app){
+  // TODO should be from location object fetched from context;
+  var channelName = getQueryParameter("channel");
+  var token = getQueryParameter("token");
+
+  app.logger.debug("initializing uplink on channel " + channelName);
+
+  var realtime = new Ably.Realtime({ token: token });
+  var channel = realtime.channels.get(channelName);
+  realtime.connection.on("connected", function(err) {
+    app.actions.uplinkAvailable();
+  });
+  realtime.connection.on("failed", function(err) {
+    app.actions.failedConnection(err.reason);
+  });
+
+  channel.publish("new Reading", "reading", function(err) {
+    if(err) {
+      console.warn("Unable to publish message; err = " + err.message);
+    } else {
+      console.info("Message successfully sent");
+    }
+  });
   var uplink = {
     startTransmission: function(){
+      console.log("opening");
     }
   };
 
--- public/lob.js
+++ public/lob.js
@@ -216,10 +216,36 @@ var Lob = (function () { 'use strict';
         };
     }
 
-    /* jshint esnext: true */
+    function getQueryParameter(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
     function default_1(app) {
+        // TODO should be from location object fetched from context;
+        var channelName = getQueryParameter("channel");
+        var token = getQueryParameter("token");
+        app.logger.debug("initializing uplink on channel " + channelName);
+        var realtime = new Ably.Realtime({ token: token });
+        var channel = realtime.channels.get(channelName);
+        realtime.connection.on("connected", function (err) {
+            app.actions.uplinkAvailable();
+        });
+        realtime.connection.on("failed", function (err) {
+            app.actions.failedConnection(err.reason);
+        });
+        channel.publish("new Reading", "reading", function (err) {
+            if (err) {
+                console.warn("Unable to publish message; err = " + err.message);
+            }
+            else {
+                console.info("Message successfully sent");
+            }
+        });
         var uplink = {
             startTransmission: function () {
+                console.log("opening");
             }
         };
         app.actions.startTransmitting.register(uplink.startTransmission);
