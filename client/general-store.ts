ed1da964f6233b7d87d0c4baed52db5310bb81ff
CrowdHailer <peterhsaxton@gmail.com>
2015-12-08 06:28:26 +0000

    general store comments

--- client/general-store.ts
+++ client/general-store.ts
@@ -3,6 +3,11 @@ function GeneralStore(): {
   state: any;
 };
 
+// State will never be assigned if evolver throws error
+// - no need for rollback;
+// Handle errors in logger?
+// - if wanted then the evolver should push errors to logger
+// Advance function to return instance of store?
 function GeneralStore(){
   var store: any;
   var state = {};
