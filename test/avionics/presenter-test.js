e86c66cfe004728972d5764871e4c7b353060343
CrowdHailer <peterhsaxton@gmail.com>
2015-12-10 16:59:58 +0000

    format max flight time

--- test/avionics/presenter-test.js
+++ test/avionics/presenter-test.js
@@ -23,24 +23,25 @@ describe("Avionics Presenter", function(){
     });
   });
 
-  // describe("in early flight state", function(){
-  //   var reading = freefallReading();
-  //   var state = {
-  //     currentFlightReadings: [reading],
-  //     flightRecords: [],
-  //     currentReading: reading
-  //   };
-  //   var presenter = AvionicsPresenter.create(state);
-  //   it("should have a maxFlightTime of .250", function(){
-  //     expect(presenter.maxFlightTime).toBe(0.250);
-  //   });
-  //   it("should have a maxAltitude of 0", function(){
-  //     expect(presenter.maxAltitude).toBe(0);
-  //   });
-  //   it("should have a current Reading of with details", function(){
-  //     expect(presenter.currentReading).toEqual("[00.00, 00.00, -1.00]");
-  //   });
-  // });
+  describe("of app early flight state", function(){
+    var reading = freefallReading(1000);
+    var appState = {
+      currentFlight: [reading],
+      flightHistory: [],
+      currentReading: reading
+    };
+    var presenter = Presenter(appState);
+
+    it("should have a maxFlightTime of .250", function(){
+      expect(presenter.maxFlightTime).toBe("0.25 s");
+    });
+    it("should have a maxAltitude of 0", function(){
+      expect(presenter.maxAltitude).toBe("0.00 m");
+    });
+    it("should have a current Reading of with details", function(){
+      expect(presenter.currentReadout).toEqual("[+00.00,  +00.00, -01.00]");
+    });
+  });
   // describe("in mid flight state", function(){
   //   var state = {
   //     currentFlightReadings: [freefallReading(100), freefallReading(200)],
