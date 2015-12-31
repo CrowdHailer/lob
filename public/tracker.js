14fc7ae7006e2c66afd7a5cd473b0de93acbe810
CrowdHailer <peterhsaxton@gmail.com>
2015-12-31 09:55:33 +0000

    setup build for tracker app

---
+++ public/tracker.js
@@ -0,0 +1,49 @@
+(function () { 'use strict';
+
+	//
+	// var Tracker = new App();
+	//
+	// // Tracker.registerService(name, Factory);
+	// Tracker.events = events;
+	// Tracker.store = store;
+	// Tracker.registerService("uplinkOptions", function(app){
+	//     return {
+	//       channelName: getName
+	//     };
+	// });
+	// Tracker.registerService("uplink", Uplink, "uplinkOptions");
+	//
+	//
+	// Tracker.contextWrapper = function(app){
+	// };
+	//
+	// function Sandbox(app, options){
+	//   var logger = Logger.wrap(app.logger, {prefix: options.name});
+	//   this.log = logger.log;
+	//   this.log = logger.log;
+	// }
+	//
+	// Tracker.registerComponent("main", function(element, app){
+	//
+	// });
+	//
+	// var tracker = tracker.start();
+	//
+	//
+	// // Urgggh
+	// Flyer.registerComponent("main", function(element, accelerometer, events, store, logger){
+	//   accelerometer.start();
+	// }, "accelerometer");
+	//
+	//
+	// Flyer.$main();
+	//
+	// events.afterAny(function(event){
+	//
+	// });
+	//
+	// action.after
+	console.log("hello");
+
+})();
+//# sourceMappingURL=tracker.js.map
