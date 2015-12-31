6d7760e0c7ace7581a69114ae7c369d15d1fff64
CrowdHailer <peterhsaxton@gmail.com>
2015-12-31 10:36:31 +0000

    proactive tracker

--- client/tracker.js
+++ client/tracker.js
@@ -1,4 +1,37 @@
 import * as URI from "./uri";
 var uri = URI.parseLocation(window.location);
-console.log(uri);
-console.log("hello")
+
+var State = {
+  fromUri: function(uri){
+    return {
+      token: uri.query["token"],
+      channelName: uri.query["channel"]
+    }
+  }
+}
+
+function Tracker(){
+  var state;
+  var tracker = this;
+  function render(projection){
+    tracker.display.render(projection);
+  }
+  this.applyState = function(newState){
+    state = newState;
+    render(state);
+  }
+}
+
+function ConsoleDisplay(logger){
+  function wrap(projection){
+    return "listening on: " + projection.channelName 
+  }
+
+  this.render = function(projection){
+    logger.info(wrap(projection));
+  }
+}
+
+var tracker = new Tracker();
+tracker.display = new ConsoleDisplay(window.console);
+tracker.applyState(State.fromUri(uri));
