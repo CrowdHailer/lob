ee660e5923967d567facea9e67615964ffbeb8c5
CrowdHailer <peterhsaxton@gmail.com>
2016-01-01 09:42:08 +0000

    spike uplinkcontroller

--- public/tracker.js
+++ public/tracker.js
@@ -165,8 +165,9 @@
 	  fromUri: function(uri){
 	    return {
 	      token: uri.query["token"],
-	      channelName: uri.query["channel"]
-	    }
+	      channelName: uri.query["channel"],
+
+	    };
 	  }
 	};
 
@@ -187,22 +188,28 @@
 	  //     // no update
 	  //   }
 	  // }
+	  this.receivedNewReaded = function(reading){
+	    console.log(reading);
+	  };
+	  this.receivedResetReadings = function(){
+	    console.log("reset reading");
+	  };
 	  this.applyState = function(newState){
 	    state = newState;
 	    updateProjection(state);
-	  }
+	  };
 
 	}
 
 	function ConsoleView(logger){
 	  function wrap(projection){
-	    return "listening on: " + projection.channel + " with token: " + projection.token
+	    return "listening on: " + projection.channel + " with token: " + projection.token;
 	    // returns presentation
 	  }
 
 	  this.render = function(projection){
 	    logger.info(wrap(projection));
-	  }
+	  };
 	}
 
 	function Projection(){
@@ -214,12 +221,12 @@
 	    projection = {
 	      channel: state.channelName,
 	      token: state.token.slice(0, 4) + "..."
-	    }
-	  }
+	    };
+	  };
 	  this.watch = function(view){
 	    view(projection);
 	    views.push(view);
-	  }
+	  };
 	}
 
 	var tracker = new Tracker();
@@ -228,8 +235,52 @@
 	// tracker.init()
 
 	var consoleView = new ConsoleView(window.console);
-	tracker.watchProjection(consoleView.render)
+	tracker.watchProjection(consoleView.render);
 	// Dom views should be initialized with the ready on certain selectors library
+
+	// function Uplink(options, logger){
+	//   var channelName = options.channel;
+	//   var token = options.token;
+	//   var realtime = new Ably.Realtime({ token: token });
+	//   var channel = realtime.channels.get(channelName);
+	//   realtime.connection.on("connected", function(err) {
+	//     console.log("realtime connected");
+	//   });
+	//   realtime.connection.on("failed", function(err) {
+	//     console.log("realtime connection failed");
+	//   });
+	// }
+
+	// var uplink = new Uplink({channel: "test", token: 123}, console);
+
+
+	// uplink controller does very little work so it is not separated from uplink
+	function UplinkController(options, tracker){
+	  var channelName = options.channel;
+	  var token = options.token;
+	  var realtime = new Ably.Realtime({ token: token });
+	  realtime.connection.on("connected", function(err) {
+	    // If we keep explicitly passing channel data to the controller we should pass it to the main app here
+	    tracker.uplinkAvailable();
+	  });
+	  realtime.connection.on("failed", function(err) {
+	    tracker.uplinkFailed();
+	  });
+	  var channel = realtime.channels.get(channelName);
+	  channel.subscribe("newReading", function(event){
+	    // new Vector(event.data);
+	    tracker.receivedNewReaded(event.data);
+	  });
+	  channel.subscribe("resetReadings", function(_event){
+	    // event information not needed
+	    tracker.receivedResetReadings();
+	  });
+	}
+
+	var uplinkController = new UplinkController({
+	  token: uri.query.token,
+	  channel: uri.query.channel
+	}, tracker);
 
 })();
 //# sourceMappingURL=tracker.js.map
