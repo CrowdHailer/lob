cbfd2dd1b64b6f8eca645c0db212500fee150aba
CrowdHailer <peterhsaxton@gmail.com>
2016-01-01 09:46:41 +0000

    separate uplink controller

--- client/tracker.js
+++ client/tracker.js
@@ -30,6 +30,9 @@ function Tracker(){
   //     // no update
   //   }
   // }
+  this.uplinkAvailable = function(){
+    console.log("uplink available");
+  };
   this.receivedNewReaded = function(reading){
     console.log(reading);
   };
@@ -80,44 +83,7 @@ var consoleView = new ConsoleView(window.console);
 tracker.watchProjection(consoleView.render);
 // Dom views should be initialized with the ready on certain selectors library
 
-// function Uplink(options, logger){
-//   var channelName = options.channel;
-//   var token = options.token;
-//   var realtime = new Ably.Realtime({ token: token });
-//   var channel = realtime.channels.get(channelName);
-//   realtime.connection.on("connected", function(err) {
-//     console.log("realtime connected");
-//   });
-//   realtime.connection.on("failed", function(err) {
-//     console.log("realtime connection failed");
-//   });
-// }
-
-// var uplink = new Uplink({channel: "test", token: 123}, console);
-
-
-// uplink controller does very little work so it is not separated from uplink
-function UplinkController(options, tracker){
-  var channelName = options.channel;
-  var token = options.token;
-  var realtime = new Ably.Realtime({ token: token });
-  realtime.connection.on("connected", function(err) {
-    // If we keep explicitly passing channel data to the controller we should pass it to the main app here
-    tracker.uplinkAvailable();
-  });
-  realtime.connection.on("failed", function(err) {
-    tracker.uplinkFailed();
-  });
-  var channel = realtime.channels.get(channelName);
-  channel.subscribe("newReading", function(event){
-    // new Vector(event.data);
-    tracker.receivedNewReaded(event.data);
-  });
-  channel.subscribe("resetReadings", function(_event){
-    // event information not needed
-    tracker.receivedResetReadings();
-  });
-}
+import UplinkController from "./tracker/uplink-controller";
 
 var uplinkController = new UplinkController({
   token: uri.query.token,
--- public/tracker.js
+++ public/tracker.js
@@ -159,6 +159,47 @@
 	  return new URI({path: path, query: query});
 	}
 
+	// Could also be called UplinkDriver - might be more suitable
+	// RESPONSIBILITY - Drive the tracker application in response to messages from the Ably uplink
+
+	/* jshint esnext: true */
+	function UplinkController(options, tracker){
+	  var channelName = options.channel;
+	  var token = options.token;
+	  var realtime = new Ably.Realtime({ token: token });
+	  realtime.connection.on("connected", function(err) {
+	    // If we keep explicitly passing channel data to the controller we should pass it to the main app here
+	    tracker.uplinkAvailable();
+	  });
+	  realtime.connection.on("failed", function(err) {
+	    tracker.uplinkFailed();
+	  });
+	  var channel = realtime.channels.get(channelName);
+	  channel.subscribe("newReading", function(event){
+	    // new Vector(event.data);
+	    tracker.receivedNewReaded(event.data);
+	  });
+	  channel.subscribe("resetReadings", function(_event){
+	    // event information not needed
+	    tracker.receivedResetReadings();
+	  });
+	}
+
+	// uplink controller does very little work so it is not separated from uplink
+
+	// function Uplink(options, logger){
+	//   var channelName = options.channel;
+	//   var token = options.token;
+	//   var realtime = new Ably.Realtime({ token: token });
+	//   var channel = realtime.channels.get(channelName);
+	//   realtime.connection.on("connected", function(err) {
+	//     console.log("realtime connected");
+	//   });
+	//   realtime.connection.on("failed", function(err) {
+	//     console.log("realtime connection failed");
+	//   });
+	// }
+
 	var uri = parseLocation(window.location);
 
 	var State = {
@@ -188,6 +229,9 @@
 	  //     // no update
 	  //   }
 	  // }
+	  this.uplinkAvailable = function(){
+	    console.log("uplink available");
+	  };
 	  this.receivedNewReaded = function(reading){
 	    console.log(reading);
 	  };
@@ -236,47 +280,6 @@
 
 	var consoleView = new ConsoleView(window.console);
 	tracker.watchProjection(consoleView.render);
-	// Dom views should be initialized with the ready on certain selectors library
-
-	// function Uplink(options, logger){
-	//   var channelName = options.channel;
-	//   var token = options.token;
-	//   var realtime = new Ably.Realtime({ token: token });
-	//   var channel = realtime.channels.get(channelName);
-	//   realtime.connection.on("connected", function(err) {
-	//     console.log("realtime connected");
-	//   });
-	//   realtime.connection.on("failed", function(err) {
-	//     console.log("realtime connection failed");
-	//   });
-	// }
-
-	// var uplink = new Uplink({channel: "test", token: 123}, console);
-
-
-	// uplink controller does very little work so it is not separated from uplink
-	function UplinkController(options, tracker){
-	  var channelName = options.channel;
-	  var token = options.token;
-	  var realtime = new Ably.Realtime({ token: token });
-	  realtime.connection.on("connected", function(err) {
-	    // If we keep explicitly passing channel data to the controller we should pass it to the main app here
-	    tracker.uplinkAvailable();
-	  });
-	  realtime.connection.on("failed", function(err) {
-	    tracker.uplinkFailed();
-	  });
-	  var channel = realtime.channels.get(channelName);
-	  channel.subscribe("newReading", function(event){
-	    // new Vector(event.data);
-	    tracker.receivedNewReaded(event.data);
-	  });
-	  channel.subscribe("resetReadings", function(_event){
-	    // event information not needed
-	    tracker.receivedResetReadings();
-	  });
-	}
-
 	var uplinkController = new UplinkController({
 	  token: uri.query.token,
 	  channel: uri.query.channel
