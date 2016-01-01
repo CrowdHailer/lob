c60fb0afc34d51ebde531992f453cad326e320ca
CrowdHailer <peterhsaxton@gmail.com>
2016-01-01 10:43:50 +0000

    setup accelerometer controller

--- client/flyer.js
+++ client/flyer.js
@@ -1,1 +1,16 @@
-console.log("oooh")
+function Flyer(){
+  this.newReading = function(reading){
+    console.log("new reading", reading);
+  };
+}
+
+var flyer = new Flyer();
+
+var DEVICEMOTION = "devicemotion";
+function AccelerometerController(global, flyer){
+  global.addEventListener(DEVICEMOTION, function(){
+    flyer.newReading();
+  });
+}
+
+var accelerometerController = new AccelerometerController(window, flyer);
--- public/flyer.js
+++ public/flyer.js
@@ -1,6 +1,21 @@
 (function () { 'use strict';
 
-	console.log("oooh")
+  function Flyer(){
+    this.newReading = function(reading){
+      console.log("new reading", reading);
+    };
+  }
+
+  var flyer = new Flyer();
+
+  var DEVICEMOTION = "devicemotion";
+  function AccelerometerController(global, flyer){
+    global.addEventListener(DEVICEMOTION, function(){
+      flyer.newReading();
+    });
+  }
+
+  var accelerometerController = new AccelerometerController(window, flyer);
 
 })();
 //# sourceMappingURL=flyer.js.map
