cd8c062a97bd334e7aea0c5743f1e22037566248
CrowdHailer <peterhsaxton@gmail.com>
2016-01-07 10:46:12 +0000

    setup needed display attributes

--- client/tracker/showcase.js
+++ client/tracker/showcase.js
@@ -1,9 +1,23 @@
 /* jshint esnext: true */
 
+function isLive(projection){
+  return !projection.flightSnapshot;
+}
+
 export default function TrackerShowcase(window){
   if ( !(this instanceof TrackerShowcase) ) { return new TrackerShowcase(window); }
 
   this.update = function(projection){
+    // Values needed in display
+    // isLive
+    // readings
+    // isLockedToLiveReadings
+    // graph lines
+    // uplink statuses
     console.log('projection', projection);
+    console.log('display', {
+      isLive: isLive(projection),
+      readings: projection.flightSnapshot || projection.liveFlight
+    });
   };
 }
--- public/tracker.js
+++ public/tracker.js
@@ -214,11 +214,25 @@ var Lob = (function () { 'use strict';
 
 	/* jshint esnext: true */
 
+	function isLive(projection){
+	  return !projection.flightSnapshot;
+	}
+
 	function TrackerShowcase(window){
 	  if ( !(this instanceof TrackerShowcase) ) { return new TrackerShowcase(window); }
 
 	  this.update = function(projection){
+	    // Values needed in display
+	    // isLive
+	    // readings
+	    // isLockedToLiveReadings
+	    // graph lines
+	    // uplink statuses
 	    console.log('projection', projection);
+	    console.log('display', {
+	      isLive: isLive(projection),
+	      readings: projection.flightSnapshot || projection.liveFlight
+	    });
 	  };
 	}
 
