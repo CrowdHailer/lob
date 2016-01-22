2076d2dedba5fc24a31cc857b653cbfca19dc3be
CrowdHailer <peterhsaxton@gmail.com>
2016-01-22 08:37:53 +0000

    fix twitter link

--- lob.rb
+++ lob.rb
@@ -42,7 +42,7 @@ end
 
 get '/flyer' do
   channel_name = request.GET["channel-name"]
-  twitter_link = "https://twitter.com/home?status=Watch%20my%20awesome%20#{base_url}/track/#{channel_name}"
+  twitter_link = "https://twitter.com/home?status=I'm%20lobbing%20my%20phone%20with%20http%3A//lob.workshop14.io/.%20Track%20my%20flight%20http%3A//lob.workshop14.io/track/#{channel_name}%20%23giveitalob"
   erb :flyer, locals: {twitter_link: twitter_link}
 end
 
