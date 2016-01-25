34376b134b7d269422494fa81317c611ec90b37b
CrowdHailer <peterhsaxton@gmail.com>
2016-01-25 09:01:04 +0000

    dissallow invalid flights

--- server/test/flight_test.rb
+++ server/test/flight_test.rb
@@ -12,4 +12,16 @@ class FlightTest < MiniTest::Test
     assert_equal flight.username, 'bob'
     assert_equal flight.max_altitude, 25
   end
+
+  def test_a_max_altitude_of_zero_is_invalid
+    assert_raises ArgumentError do
+      flight = Flight.new(username: 'bob', max_altitude: 0)
+    end
+  end
+
+  def test_a_blank_username_is_invalid
+    assert_raises ArgumentError do
+      flight = Flight.new(username: '', max_altitude: 9)
+    end
+  end
 end
