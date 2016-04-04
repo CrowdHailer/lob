9467255500fac7e7a5d3580a8f9ba17a4d95ff05
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-04-04 04:45:00 +0200

    Performance: Revert to Chart.js as AMCharts too CPU intensive
    - Mobile devices could not keep up with the display of the charts, so reverted to AMCharts. See http://jsperf.com/amcharts-vs-highcharts-vs-canvasjs/5 why.
    - Charts don’t look as good or provide as much info, but performance is greatly improved

--- client/tracker/graph-display.js
+++ client/tracker/graph-display.js
@@ -3,139 +3,123 @@ import { Config } from '../config';
 export default function GraphDisplay(trackDivId) {
   if ( !(this instanceof GraphDisplay) ) { return new GraphDisplay(trackDivId); }
 
-  var chart;
+  var chart,
+      context,
+      data = [],
+      lineDataTemplate = function() { return {
+        labels: [],
+        datasets: [
+          {
+            title: "Magnitude",
+            fillColor: "rgba(151,187,205,0)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointstrokeColor : "yellow",
+            data: [],
+            xPos: [],
+            axis: 1
+          },
+          {
+            title: "Throw",
+            type: "Line",
+            fill: true,
+            fillColor: "rgba(220,0,0,0.6)",
+            strokeColor: "rgba(220,0,0,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointstrokeColor : "yellow",
+            data: [],
+            xPos: [],
+            axis: 2
+          }
+        ]
+      }; },
+      chartOptions = {
+        datasetFill: true,
+        animation: false,
+        pointDot : false,
+        datasetFill: false,
+        showToolTips: false,
+        scaleOverride: true,
+        scaleStartValue: -10,
+        scaleSteps: 8,
+        scaleStepWidth: 10,
+        scaleLabel: "<%=value%>",
+        responsive: true,
+        maintainAspectRatio: true,
+        yAxisLabel: "LobForce™",
+        showXLabels: 5,
+        scaleXGridLinesStep: 5,
+        fmtXLabel : "fmttime ss",
+        extrapolateMissingData: false,
+        inGraphDataShow: true,
+        inGraphDataTmpl: "<%=((v1 == 'Throw') && (v3 > 0) ? 'Lob ' + Math.round(v3*100)/100 + 'm' : '')%>",
+        inGraphDataAlign: "left",
+        inGraphDataVAlign: "bottom",
+        inGraphDataPaddingX: 20,
+        inGraphDataPaddingY: -15,
+        inGraphDataFontColor: "rgba(220,0,0,1)"
+      };
 
-  function initialize() {
-    var chartOptions = {
-      "type": "serial",
-      "theme": "light",
-      "borderColor": "#FFFFFF",
-      "dataProvider": [],
-      "marginTop": 10,
-      "marginRight": 0,
-      "marginLeft": 0,
-      "marginBottom": 0,
-      "zoomOutButtonAlpha": 1,
-      "zoomOutText": "",
-      "valueAxes": [{
-        "id": "forceaxis",
-        "autoGridCount": true,
-        "axisAlpha": 0.2,
-        "dashLength": 1,
-        "position": "left",
-        "maximum": 100,
-        "minimum": -10,
-        "strictMinMax": true,
-        "title": "LobForce™",
-        "axisTitleOffset": 5
-      },{
-        "id": "altitudeaxis",
-        "baseValue": 0,
-        "axisColor": "#00FF00",
-        "position": "right",
-        "axisAlpha": 1,
-        "gridAlpha": 0,
-        "axisThickness": 2,
-        "unit": "m",
-        "title": "Altitude in metres",
-        "axisTitleOffset": 5
-      }],
-      "mouseWheelZoomEnabled": true,
-      "graphs": [{
-        "id": "force",
-        "balloonText": "LobForce™ [[value]]",
-        "title": "LobForce™",
-        "valueField": "value",
-        "valueAxis": "forceaxis",
-        "lineColor": "#d1655d",
-        "lineThickness": 2,
-        "negativeLineColor": "#637bb6",
-        "type": "smoothedLine",
-        "balloon":{
-          "drop": true
-        }
-      }, {
-        "id": "altitude",
-        "balloonText": "Altitude [[value]]m",
-        "title": "Altitude (m)",
-        "stackType": "regular",
-        "valueField": "altitude",
-        "valueAxis": "altitudeaxis",
-        "lineColor": "#00FF00",
-        "lineThickness": 2,
-        "type": "smoothedLine",
-        "fillAlphas": 0.6,
-        "lineAlpha": 0.4,
-        "balloon":{
-          "drop": true
-        }
-      }],
-      "chartCursor": {
-         "categoryBalloonFunction": function(valueText) {
-          var date = new Date(Number(valueText));
-          return AmCharts.formatDate(date, "HH:NN:SS.QQQ");
-        }
-      },
-      "categoryField": "date",
-      "categoryAxis": {
-        "parseDates": false,
-        "axisColor": "#DADADA",
-        "dashLength": 1,
-        "minorGridEnabled": false,
-        "labelFunction": function(valueText, data, categoryAxis) {
-          var date = new Date(Number(valueText));
-          return date.getSeconds().toString() + '.' + Math.round(date.getMilliseconds() / 100).toString();
-        }
-      },
-      "export": {
-        "enabled": false
-      }
-    }
-
-    chart = AmCharts.makeChart(trackDivId, chartOptions);
-    chart.addListener("dataUpdated", zoomChart);
-
-    chart.addListener( "init", function() {
-      chart.categoryAxis.addListener( "rollOverItem", function( event ) {
-          event.target.setAttr( "cursor", "default" );
-          event.chart.balloon.followCursor( true );
-          event.chart.balloon.showBalloon( event.serialDataItem.dataContext.key + " - " + event.serialDataItem.dataContext.value );
-      });
-      chart.categoryAxis.addListener( "rollOutItem", function( event ) {
-          event.chart.balloon.hide();
-      });
-    })
+  function initialize(lineData) {
+    var canvas = $('#tracker-graph canvas');
+    context = canvas[0].getContext("2d");
+    chart = new Chart(context).Line(lineData, chartOptions);
   }
 
   function maxTimestampFromData() {
-    return Math.max.apply(null, chart.dataProvider.map(function(elem) { return elem.date; }));
+    return Math.max.apply(null, data.map(function(elem) { return elem.date; }));
   }
 
-  function zoomChart() {
-    var maxTimestampInData,
-        minTimestampInData,
-        minAllowedTimestamp,
-        data = chart.dataProvider;
+  /*
+    Sort data, then delete old data, cut off point of Config.trackingGraphTimePeriod
+  */
+  function prepareAndTruncateData() {
+    var minDateOnGraph = maxTimestampFromData() - Config.trackingGraphTimePeriod,
+        lineData = lineDataTemplate();
 
-    maxTimestampInData = maxTimestampFromData();
-    minAllowedTimestamp = maxTimestampInData - Config.trackingGraphTimePeriod;
-    minTimestampInData = Math.min.apply(null, data.map(function(elem) {
-      return elem.date;
-    }).filter(function(date) {
-      return date >= minAllowedTimestamp;
-    }));
+    data = data.filter(function(point) {
+      return point.date > minDateOnGraph;
+    }).sort(function(a,b) {
+      return a.date - b.date;
+    });
 
-    if (maxTimestampInData && minTimestampInData) {
-      chart.zoomToCategoryValues(minTimestampInData.toString(), maxTimestampInData.toString());
+    lineData.labels = data.map(function(point) {
+      return point.date;
+    });
+
+    var magnitudeData = data.filter(function(point) { return !isNaN(parseInt(point.value)) });
+    lineData.datasets[0].data = magnitudeData.map(function(point) {
+      return point.value;
+    });
+    lineData.datasets[0].xPos = magnitudeData.map(function(point) {
+      return point.date;
+    });
+
+    var flightData = data.filter(function(point) { return isNaN(parseInt(point.value)) });
+    lineData.datasets[1].data = flightData.map(function(point) {
+      return point.altitude;
+    });
+    lineData.datasets[1].xPos = flightData.map(function(point) {
+      return point.date;
+    });
+    if (!lineData.datasets[1].xPos.length) {
+      delete lineData.datasets[1].xPos;
+    }
+
+    if (!chart) {
+      initialize(lineData);
+    } else {
+      updateChart(context, lineData, chartOptions);
     }
   }
 
   this.addPoint = function(point) {
-    chart.dataProvider.push({
-      date: point.timestamp,
-      value: Math.round((point.magnitude - Config.gravityMagnitudeConstant) * 100) / 100 /* LobForce is stationery at 0 */
+    var lobForce = Math.round((point.magnitude - Config.gravityMagnitudeConstant) * 100) / 100; /* LobForce is stationery at 0 */
+    data.push({
+      date: new Date(point.timestamp),
+      value: lobForce
     });
-    this.prepareAndTruncateData();
+    prepareAndTruncateData();
   }
 
   this.addFlight = function(flightData) {
@@ -143,52 +127,28 @@ export default function GraphDisplay(trackDivId) {
     var end = flightData.peakInfo[flightData.peakInfo.length-1].timestampEnd;
     var altitude = flightData.altitude;
     var midpointDate = (start + end) / 2;
-    var guide = new AmCharts.Guide();
 
-    chart.dataProvider.push({
-      date: start,
+    data.push({
+      date: new Date(start - 10),
+      altitude: undefined
+    });
+    data.push({
+      date: new Date(start),
       altitude: 0
     });
-    chart.dataProvider.push({
-      date: midpointDate,
+    data.push({
+      date: new Date(midpointDate),
       altitude: altitude
     });
-    chart.dataProvider.push({
-      date: end,
+    data.push({
+      date: new Date(end),
       altitude: 0
     });
-
-    guide.label = "Lob " + Math.round(altitude * 100)/100 + "m, airborne " + Math.round(flightData.flightTime * 100)/100 + "s";
-    guide.labelRotation = 90;
-    guide.position = "top";
-    guide.inside = true;
-    guide.color = "black";
-    guide.lineThickness = 2;
-    guide.lineAlpha = 1;
-    guide.category = midpointDate;
-    chart.categoryAxis.addGuide(guide);
-
-    this.prepareAndTruncateData();
-  }
-
-  /* Sort data, then delete old data, but not exactly on the cut off in case there are curves
-     that span the cut off point of Config.trackingGraphTimePeriod */
-  this.prepareAndTruncateData = function() {
-    var data = chart.dataProvider,
-        minDateOnGraph = maxTimestampFromData() - Config.trackingGraphTimePeriod * 2;
-
-    chart.dataProvider = data.filter(function(point) {
-      return point.date > minDateOnGraph;
-    }).sort(function(a,b) {
-      return a.date - b.date;
+    data.push({
+      date: new Date(end + 10),
+      altitude: undefined
     });
 
-    chart.guides = chart.guides.filter(function(guide) {
-      return guide.category > minDateOnGraph;
-    });
-
-    chart.validateData();
+    prepareAndTruncateData();
   }
-
-  initialize();
 }
