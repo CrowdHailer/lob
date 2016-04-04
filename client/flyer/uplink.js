43c75ddd62d9c7ad18fa5d5e17d81e3c07e06bbf
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-04-04 17:45:02 +0200

    Ensure channel name is a string and upcased

--- client/flyer/uplink.js
+++ client/flyer/uplink.js
@@ -8,10 +8,10 @@ export default function FlyerUplink(options, logger) {
   logger.info('Starting uplink', options);
 
   var uplink = this;
-  var channelName = options.channelName;
+  var channelName = options.channelName.toString();
 
   // TODO: Remove clientId when https://github.com/ably/ably-js/issues/252 resolved
-  var client = new Ably.Realtime({ authUrl: '/flyer/' + channelName + '/token', clientId: channelName });
+  var client = new Ably.Realtime({ authUrl: '/flyer/' + channelName + '/token', clientId: channelName.toString() });
   var channel = client.channels.get(channelName);
 
   /* Flights namespace is configured to persist messages */
