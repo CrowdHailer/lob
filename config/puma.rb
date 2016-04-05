a00242c90f9740fc675734dd9b38ee5d455e5ae0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-04-05 15:15:27 +0200

    Fix threading issue sharing a connection after Puma fork
    See https://devcenter.heroku.com/articles/postgres-logs-errors#pgerror-ssl-syscall-error-eof-detected

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,16 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+FORKS = Integer(ENV['WEB_CONCURRENCY'] || 2)
 
-preload_app!
+workers FORKS
+threads_count = Integer(ENV['MAX_THREADS'] || 10)
+threads threads_count, threads_count
 
 port        ENV['PORT']     || 5000
 environment ENV['RACK_ENV'] || 'development'
+
+preload_app!
+
+on_worker_boot do
+  # Disconnect the Sequel connection pool so new connections are created.
+  DB.disconnect
+  DB = Sequel.connect(DATABASE_URL, max_connections: (ENV['MAX_DATABASE_CONNECTIONS'] || 20) / FORKS)
+end
